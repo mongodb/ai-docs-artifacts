bf33a8c19902f8a7683cf88ee739414817add6ae
Chris Cho <chris.cho@10gen.com>
2024-02-14 15:18:33 -0500

    updateMany updates

--- code-examples/php/usage-examples/updateMany.php
+++ code-examples/php/usage-examples/updateMany.php
@@ -1,10 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
-// Replace the uri string with your MongoDB deployment's connection string.
-$uri = "<connection string uri>";
 
-$client = new MongoDB\Client($uri);
 
 abstract class Rating {
     const G = "G";
@@ -14,21 +11,25 @@ abstract class Rating {
     const NR = "NOT RATED";
 }
 
-try {
-    // Get the "movies" collection in the "sample_mflix" database
-    $db = $client->sample_mflix;
+// Replace the uri string with your MongoDB deployment's connection string.
+$uri = "mongodb://localhost:27017";
 
+$client = new MongoDB\Client($uri);
+
+// Get the "movies" collection in the "sample_mflix" database
+$collection = $client->sample_mflix->movies;
+
+try {
     // Update all documents that match the specified filter
-    $result = $db->movies->updateMany(
+    $result = $collection->updateMany(
         ['rated' => Rating::G],
         ['$set' => ['random_review' => "After viewing I am ".(rand(0,100))."% more satisfied with life."]]
     );
 
-    echo "Updated ".$result->getModifiedCount()." documents\n";
+    print_r("Updated ".$result->getModifiedCount()." documents\n");
+
 } catch (Exception $e) {
     echo $e->getMessage(), "\n";
-} finally {
-    // Close the database connection on completion or error
-    $client->close();
 }
+
 ?>
