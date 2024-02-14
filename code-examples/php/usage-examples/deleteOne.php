f7bc2ae2853e31d3031b2d497b968a0e4ec13abb
Chris Cho <chris.cho@10gen.com>
2024-02-14 13:44:59 -0500

    deleteOne updates

--- code-examples/php/usage-examples/deleteOne.php
+++ code-examples/php/usage-examples/deleteOne.php
@@ -2,10 +2,10 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 // Replace the uri string with your MongoDB deployment's connection string
-$uri = "<connection string uri>";
+$uri = "mongodb://localhost:27017";
 
 $client = new MongoDB\Client($uri);
-$movies = $client->sample_mflix->movies;
+$collection = $client->sample_mflix->movies;
 
 // Delete the first document in the "movies" collection that matches
 // the specified query document 
@@ -13,18 +13,15 @@ $filter = ['title' => 'Annie Hall'];
 $options = ['limit' => 1];
 
 try {
-    $deleteResult = $movies->deleteOne($filter, $options);
-    // Print a message that indicates whether the operation deleted a
-    // document 
+    $deleteResult = $collection->deleteOne($filter, $options);
+
     if ($deleteResult->getDeletedCount() === 1) {
-        printf("Successfully deleted one document.\n");
+        print("Successfully deleted one document.\n");
     } else {
-        printf("No documents matched the query. Deleted 0 documents.\n");
+        print("No documents matched the query. Deleted 0 documents.\n");
     }
 } catch(MongoDB\Driver\Exception\Exception $e) {
     printf("Error: %s\n", $e->getMessage());
 }
 
-// Close the connection after the operation completes
-$client = null;
 ?>
