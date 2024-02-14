3b5f3861fa25d4dd2843c41eb75af281f28c91d4
Chris Cho <chris.cho@10gen.com>
2024-02-14 13:15:59 -0500

    bulkWrite updates

--- code-examples/php/usage-examples/bulkWrite.php
+++ code-examples/php/usage-examples/bulkWrite.php
@@ -3,57 +3,41 @@
 require 'vendor/autoload.php'; 
 
 // Replace the uri string with your MongoDB deployment's connection string.
-$uri = "<connection string uri>";
+$uri = "mongodb://localhost:27017";
 
-$client = new MongoDB\Client($uri);
-$database = $client->sample_mflix;
-$theaters = $database->theaters;
+$collection = (new MongoDB\Client($uri))->sample_mflix->movies;
 
 try {
-    $result = $theaters->bulkWrite([
-        [
-            'insertOne' => [
-                'document' => [
-                    'location' => [
-                        'address' => [
-                            'street1' => "3 Main St.",
-                            'city' => "Anchorage",
-                            'state' => "AK",
-                            'zipcode' => "99501",
-                        ]
-                    ],
+    $result = $collection->bulkWrite([
+        [ 'insertOne' => [[
+                'location' => [
+                    'address' => [
+                        'street1' => '3 Main St.',
+                        'city' => 'Anchorage',
+                        'state' => 'AK',
+                        'zipcode' => '99501',
+                    ]
                 ]
-            ]
+            ]]
         ],
-        [
-            'insertOne' => [
-                'document' => [
-                    'location' => [
-                        'address' => [
-                            'street1' => "75 Penn Plaza",
-                            'city' => "New York",
-                            'state' => "NY",
-                            'zipcode' => "10001",
-                        ]
-                    ],
+        [ 'insertOne' => [[
+                'location' => [
+                    'address' => [
+                        'street1' => '75 Penn Plaza',
+                        'city' => 'New York',
+                        'state' => 'NY',
+                        'zipcode' => '10001',
+                    ]
                 ]
-            ]
+            ]]
         ],
-        [
-            'updateMany' => [
-                'filter' => [ 'location.address.zipcode' => "44011" ],
-                'update' => [ '$set' => [ 'is_in_ohio' => true ] ],
-                'upsert' => true,
-            ]
-        ],
-        [
-            'deleteOne' => [
-                'filter' => ['location.address.street1' => "221b Baker St"],
-            ],
-        ],
-    ]);
+        [ 'deleteOne' => [[ 'location.address.street1' => '221b Baker St' ]]]
+      ],
+    );
 
-    var_dump($result);
+    print_r("Inserted: " . $result->getInsertedCount() . "\n");
+    print_r("Removed: " .  $result->getDeletedCount() . "\n");
+    //var_dump($result);
 
 } catch (MongoDB\Driver\Exception\Exception $e) {
     echo $e->getMessage(), "\n";
