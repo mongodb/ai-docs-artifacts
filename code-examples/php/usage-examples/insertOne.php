0386e5cf6ed61bcd069568219e81cb3e5da7ac70
Chris Cho <chris.cho@10gen.com>
2024-02-14 14:42:41 -0500

    insertOne updates

--- code-examples/php/usage-examples/insertOne.php
+++ code-examples/php/usage-examples/insertOne.php
@@ -4,25 +4,28 @@ require_once __DIR__ . "/vendor/autoload.php";
 class Haiku {
   public $title;
   public $content;
-  
+
   function __construct($title, $content){
     $this->title = $title;
     $this->content = $content;
-  }   
+  }
 }
 
-$uri = "<connection string uri>";
+$uri = "mongodb://localhost:27017";
 $client = new MongoDB\Client($uri);
-$database = $client->insertDB;
-$haiku = $database->haiku;
+
+$collection = $client->poetry->haiku;
 
 try {
-  $newHaiku = new Haiku("Record of a Shriveled Datum", "No bytes, no problem. Just insert a document, in MongoDB");
-  $insertResult = $haiku->insertOne($newHaiku);
-  echo "A document was inserted with the _id:" . $insertResult->getInsertedId();
+    $newHaiku = new Haiku(
+        "Record of a Shriveled Datum", 
+        "No bytes, no problem. Just insert a document, in MongoDB"
+    );
+
+    $result = $collection->insertOne($newHaiku);
+
+    print("A document was inserted with the _id:" . $result->getInsertedId());
 } catch(Exception $e){
-  echo $e->getMessage();
-} finally {
-  $client = null;
+    echo $e->getMessage();
 }
 ?>
