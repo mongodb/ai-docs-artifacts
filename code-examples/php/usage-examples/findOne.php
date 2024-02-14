e5f5e7264077a7e2506d96ba5bbf4c5af53153e6
Chris Cho <chris.cho@10gen.com>
2024-02-14 14:29:41 -0500

    findOne updates

--- code-examples/php/usage-examples/findOne.php
+++ code-examples/php/usage-examples/findOne.php
@@ -2,55 +2,27 @@
 
 require 'vendor/autoload.php';
 
-use MongoDB\Client;
-use MongoDB\BSON\UTCDateTime;
 
 // Replace the uri string with your MongoDB deployment's connection string.
-$uri = "<connection string uri>";
+$uri = "mongodb://localhost:27017";
 
-$client = new Client($uri);
+$client = new MongoDB\Client($uri);
 
-class IMDB {
-    public $rating;
-    public $votes;
-    public $id;
+try {
+    $collection = $client->sample_mflix->movies;
+    $filter = ['title' => 'The Room'];
+    $options = [
+         'sort' => ['rating' => -1],
+         'projection' => ['_id' => 0, 'title' => 1, 'imdb' => 1],
+    ];
+
+
+    $movie = $collection->findOne($filter, $options);
+
+    print_r($movie);
+
+} catch (Exception $e) {
+    error_log($e->getMessage());
 }
-
-class Movie {
-    public $title;
-    public $year;
-    public $released;
-    public $plot;
-    public $type;
-    public $imdb;
-}
-
-class MovieSummary {
-    public $title;
-    public $imdb;
-}
-
-function run() {
-    global $client;
-    try {
-        $database = $client->sample_mflix;
-        $movies = $database->movies;
-
-        $movie = $movies->findOne(
-            ['title' => 'The Room'],
-            [
-                'sort' => ['rating' => -1],
-                'projection' => ['_id' => 0, 'title' => 1, 'imdb' => 1],
-            ]
-        );
-        print_r($movie);
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-    } finally {
-        $client->close();
-    }
-}
-
-run();
 
 ?>
